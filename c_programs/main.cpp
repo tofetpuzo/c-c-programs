2cc4246cfa1763fdd737c49938567a8feb5155be
tofetpuzo <topetemiola09@gmail.com>
2023-02-14 23:29:49 +0000

    adding numbers

--- c_programs/main.cpp
+++ c_programs/main.cpp
@@ -5,9 +5,10 @@ using namespace std;
 int main()
 {
     int a, b, c;
-    cout << "Enter two numbers";
+    cout << "Enter a and b numbers";
     cin >> a >> b;
     c = a + b;
     cout << "addition of the code is " << c;
     return 0;
+
 }
