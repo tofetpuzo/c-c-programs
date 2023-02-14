4981453575190bda91994132aa28d2a589bc71f6
tofetpuzo <topetemiola09@gmail.com>
2023-02-14 23:26:27 +0000

    adding numbers

---
+++ c_programs/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "Enter two numbers";
+    cin >> a >> b;
+    c = a + b;
+    cout << "addition of the code is " << c;
+    return 0;
+}
