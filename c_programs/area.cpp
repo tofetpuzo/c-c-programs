9af832e9d77b12b195a78f6206f6e5fdeb8a019a
tofetpuzo <topetemiola09@gmail.com>
2023-02-15 20:45:30 +0000

    adding numbers

--- c_programs/area.cpp
+++ c_programs/area.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 
-// int main() {
-//     std::cout << "Hello World!";
-//     return 0;
-// }
 
 #include <iostream>
 #include <cmath>
 using namespace std;
 
+// Area of a circle.
 int main()
 {
     float radius;
     float area;
 
-    cout << "Please provide the radius of the circle you wish to calculate?";
+    cout << "Please provide the radius of the circle you wish to calculate?  ";
     cin >> radius;
     area = 3.142 * pow(radius, 2);
 
