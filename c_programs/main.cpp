463f2562fda9ad9643603b636ebdd02ada78c749
tofetpuzo <topetemiola09@gmail.com>
2023-02-20 13:04:03 +0000

    using bitwise

--- c_programs/main.cpp
+++ c_programs/main.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
-    cout << "Enter a and b numbers";
-    cin >> a >> b;
-    c = a + b;
-    cout << "addition of the code is " << c;
-    return 0;
+    // int a, b, c;
+    // cout << "Enter a and b numbers";
+    // cin >> a >> b;
+    // c = a + b;
+    // cout << "addition of the code is " << c;
+    // return 0;
+   
+    double x = 2 * 3.9 + ('A' * 5UL);
+   int a=25 , b=9 ;
+    cout<<(~a); 
+    cout<<(a | b);
+    // cout << x;
 };
