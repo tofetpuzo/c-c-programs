f0dd08b0540399f461450130d82a741d746ac793
tofetpuzo <topetemiola09@gmail.com>
2023-11-05 17:26:24 +0000

    partial evaluation lambda

--- ude_class/algorithm/var_in_lambda.cc
+++ ude_class/algorithm/var_in_lambda.cc
@@ -5,15 +5,28 @@
 
 using namespace std;
 
+auto greeter(const string &saturation) {
+  return [saturation](const string &name) { return saturation + ", "s + name; };
+}
+
 int main() {
   vector<string> words{"a", "james", "eat", "apple", "banana", "orange"};
   int n{5}, idx{-1}; // add another variable idx for the index of the first
                      // element that satisfies the condition
+                     //   int max{5};
+
+  //   auto is_longer_than = [max](const string &s) { return s.size() > max; };
+
+  //   auto res = find_if(cbegin(words), cend(words), is_longer_than);
 
   auto res = find_if(cbegin(words), cend(words), [n, &idx](const string &s) {
     ++idx;
     return s.size() > n;
   });
+
+  //   great function
+  auto greet = greeter("Hello"s);
+  cout << greet("James"s) << endl;
 
   if (res != cend(words)) {
     cout << R"(The first word with more than )" << n << R"( characters is ")";
