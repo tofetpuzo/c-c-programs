80a7bbe68ecba829b442ee725a0dd274375139a7
tofetpuzo <topetemiola09@gmail.com>
2023-11-18 11:06:27 +0000

    using vs code debugging tool

---
+++ sbg/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        text = ""
+        for i in range(10):
+            text += str(i) + " "
+            
+        return render_template('index.html', **locals())
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
+
+
