1ef000058ab44e411e4341ff197a5ff25bc31812
tofetpuzo <topetemiola09@gmail.com>
2024-04-01 13:37:51 +0100

    update ignore

--- ude_class/containers/map/maps.cpp
+++ ude_class/containers/map/maps.cpp
@@ -4,26 +4,25 @@
 
 using namespace std;
 
-void printMap(const map<string, int> &scores)
-{
-    cout << "Map contains: " << endl;
-    for (auto it : scores)    // use a range-based for loop to iterate over the map
-    {
-        cout << it.first << " has a score of " << it.second << endl;
-    }
+void printMap(const map<string, int> &scores) {
+  cout << "Map contains: " << endl;
+  for (auto it : scores) // use a range-based for loop to iterate over the map
+  {
+    cout << it.first << " has a score of " << it.second << endl;
+  }
 
-    cout << endl;
+  cout << endl;
 }
 
-int main(){
-    map<string, int> scores;
-    scores.insert(make_pair("John", 100));
-    scores.insert(make_pair("Doe", 200));
-    scores.insert(make_pair("Jane", 300));
-    scores.insert(make_pair("Smith", 400));
-    scores.insert(make_pair("Doe", 500));
+int main() {
+  map<string, int> scores;
+  scores.insert(make_pair("John", 100));
+  scores.insert(make_pair("Doe", 200));
+  scores.insert(make_pair("Jane", 300));
+  scores.insert(make_pair("Smith", 400));
+  scores.insert(make_pair("Doe", 500));
 
-    printMap(scores);
+  printMap(scores);
 
-    cout << "adding element with key John and value 100" << endl;
+  cout << "adding element with key John and value 100" << endl;
 }
