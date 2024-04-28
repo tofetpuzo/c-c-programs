dbf751b2c8a2baf4b3c5e979148324c727239481
tofetpuzo <topetemiola09@gmail.com>
2024-04-29 00:52:31 +0100

    exceptions in c++

--- c_programs/dependencies_hell_js/modA.js
+++ c_programs/dependencies_hell_js/modA.js
@@ -1,29 +1,27 @@
-const exp = require('constants');
-const fs = require('fs');
-const myModule = (() => {
-    const privateGetName = () => { };
-    const privateGetLocation = [];
-
-    const exported = {
-        publicGetName: () => { },
-        publicGetLocation: () => { }
-    }
-    return exported
-});
+const exp = require("constants");
+const fs = require("fs");
+const myModule = () => {
+  const exported = {
+    publicGetName: () => {},
+    publicGetLocation: () => {},
+  };
+  return exported;
+};
 
 console.log(myModule);
 console.log(myModule.privateGetLocation, myModule.privateGetName);
 
 function loadModules(filename, module, require) {
-    const wrappedSrc = `(function (module, exports, require) {
-        ${fs.readFileSync(filename, 'utf8')}
-        (module, module.exports, require)`
-    eval(wrappedSrc)
+  const wrappedSrc = `(function (module, exports, require) {
+        ${fs.readFileSync(filename, "utf8")}
+        (module, module.exports, require)`;
+  eval(wrappedSrc);
 }
 
 exports.loaded = false;
-const b = require('./modB');
+const b = require("./modB");
 
 module.exports = {
-    b, loaded: true
-}
+  b,
+  loaded: true,
+};
