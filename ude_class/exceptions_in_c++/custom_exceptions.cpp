3b8108a2577a729cfc0acaf65e181fd1335cbc21
tofetpuzo <topetemiola09@gmail.com>
2024-05-05 14:46:31 +0100

    custom exceptions for student grades

--- ude_class/exceptions_in_c++/custom_exceptions.cpp
+++ ude_class/exceptions_in_c++/custom_exceptions.cpp
@@ -15,9 +15,45 @@ public:
     bad_student_grade(const char *msg) : std::out_of_range(msg) {}
 
     // These default operators are good enough as we do not have any data members
-    bad_student_grade(const bad_student_grade &) = default;
-    bad_student_grade &operator=(const bad_student_grade &) = default;
+    bad_student_grade(const bad_student_grade &other) = default;
+    bad_student_grade &operator=(const bad_student_grade &other) = default;
 
     // finally, we can override the virtual what() member function
     // const char* what() const noexcept override { return "Invalid grad: please try again"; }
 };
+
+class StudentGrade
+{
+    int grade;
+
+public:
+    StudentGrade(int grade) : grade(grade)
+    {
+        if (grade < 0)
+        {
+            throw bad_student_grade("bad grade");
+        }
+
+        if (grade > 100)
+        {
+            throw bad_student_grade();
+        }
+    }
+};
+
+int main()
+{
+    int result;
+    cout << "Enter student grade: ";
+    cin >> result;
+    try
+    {
+        StudentGrade student(result);
+        cout << "Student grade is: " << result << endl;
+    }
+    catch (bad_student_grade &e)
+    {
+        cout << "Caught a bad student grade exception: " << e.what() << endl;
+    }
+    return 0;
+}
