c7b3322a9ef65d6e4669d78bcc93146e2c52ce0a
tofetpuzo <topetemiola09@gmail.com>
2024-05-12 14:20:56 +0100

    move semantics

--- ude_class/move_semantics/move_operators.cpp
+++ ude_class/move_semantics/move_operators.cpp
@@ -2,38 +2,68 @@
 
 using namespace std;
 
-class Grade
-{
-};
+class Grade {};
 
 // class with move constructor
-class Test
-{
+class Test {
 private:
-    int grade{0};
-    Grade grade_obj;
+  int grade{0};
+  Grade grade_obj;
 
 public:
-    Test() = default;
-    Test(int grade) : grade(grade) {}
-    Test(const Test &other) = default;
-    Test &operator=(const Test &other) = default;
-    Test(Test &&other) noexcept
-    {
-        cout << "Move constructor" << endl;
-        grade = other.grade;
-        other.grade = 0;
+  Test() = default;
+
+  // copy constructor
+  Test(const Test &arg) : grade(arg.grade), grade_obj(arg.grade_obj) {
+    cout << "Copy constructor called" << endl;
+  }
+
+  // move constructor
+  Test(Test &&arg) noexcept : grade(arg.grade), grade_obj(move(arg.grade_obj)) {
+    cout << "Move constructor called" << endl;
+  };
+
+  // copy assignment operator
+  Test &operator=(const Test &arg) {
+    cout << "copy Assignment operator called" << endl;
+    if (this != &arg) {
+      grade = arg.grade;
+      grade_obj = arg.grade_obj;
     }
-    Test &operator=(Test &&other) noexcept
-    {
-        cout << "Move assignment" << endl;
-        grade = other.grade;
-        other.grade = 0;
-        return *this;
+    return *this;
+  }
+  //   Move assignment operator
+  Test &operator=(Test &&arg) noexcept {
+    cout << "Move Assignment operator called" << endl;
+    if (this != &arg) {
+      grade = arg.grade;
+      grade_obj = move(arg.grade_obj); // force move assignment to be called
     }
-    void print() const
-    {
-        cout << grade << endl;
-    }
+    return *this;
+  }
+
+  void print() { cout << "Grade: " << grade << endl; }
 };
 
+int main() {
+  Test test;
+  cout << "Copying:  ";
+
+  Test test2 = test;
+
+  cout << "\nMoving temporay:  ";
+  Test test3 = Test(); // move constructor called
+
+  cout << "\nMoving rvalue ref:  ";
+  Test test4(std::move(test)); // move constructor called
+
+  cout << endl;
+
+  Test test5;
+  cout << "\nAssigning:  ";
+  test5 = test2; // copy assignment operator called
+
+  Test test6;
+  cout << "\nassigning from temporary:  ";
+  test6 = Test(); // move assignment operator called
+}
